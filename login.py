23b93e8c8d6f058d121d366410800cac8cdd703a
MANIMSV <manikandan.2002121@srec.ac.in>
2025-01-28 09:48:04 +0530

    feat: Implement login functionality with success and failure messages

--- login.py
+++ login.py
@@ -8,6 +8,19 @@ class login_form(qtw.QWidget, Ui_w_loginform):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.pushButton_ok.clicked.connect(self.on_pushButton_ok_clicked)
+        self.pushButton_cancel.clicked.connect(self.close)
+
+    @qtc.Slot()
+    def on_pushButton_ok_clicked(self):
+        username = self.lineedit_userid.text()
+        password = self.lineedit_password.text()
+
+        if username == "Manikandan" and password == "Mani2503@":
+            self.label_message.setText("Login Successful")
+        else:
+            self.label_message.setText("Login Failed")
+
 
 if __name__ == "__main__":
     app = qtw.QApplication(sys.argv)
