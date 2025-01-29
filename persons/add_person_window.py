4f5c74c9b72e3230532e986d6b488a295b3f84be
MANIMSV <manikandan.2002121@srec.ac.in>
2025-01-29 22:21:34 +0530

    feat: Implement add person dialog with entry processing and success message

---
+++ persons/add_person_window.py
@@ -0,0 +1,26 @@
+import sys
+import random
+from PySide6 import QtCore as qtc, QtWidgets as qtw, QtGui as qtg
+
+from UI.add_person_window_ui import Ui_d_persons
+
+class add_person(qtw.QDialog, Ui_d_persons):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pb_close.clicked.connect(self.reject)
+        self.pb_submit.clicked.connect(self.process_entry)
+
+    @qtc.Slot()
+    def process_entry(self):
+        firstname = self.lineEdit_firstname.text()
+        lastname = self.lineEdit_lastname.text()
+        self.label_message.setText("Person Added Successfully")
+        self.lineEdit_firstname.clear()
+        self.lineEdit_lastname.clear()
+
+if __name__ == "__main__":
+    app = qtw.QApplication(sys.argv)
+    form = add_person()
+    form.open()
+    sys.exit(app.exec())
